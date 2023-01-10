8673aecf7e3f995113b84fea29b5b0d7acea2e65
Jeroen Sak <jeroen.sak@hotmail.com>
2023-01-10 14:56:35 +0100

    remove clippath

--- components/page-wrapper/page-wrapper.tsx
+++ components/page-wrapper/page-wrapper.tsx
@@ -19,17 +19,14 @@ export const PageWrapper = ({
       initial={{
         opacity: 0,
         y: 20,
-        clipPath: "polygon(0 0, 100% 0, 100% 100%, 0 100%)",
       }}
       animate={{
         opacity: 1,
         y: 0,
-        clipPath: "polygon(0 0, 100% 0, 100% 100%, 0 100%)",
       }}
       exit={{
         opacity: 0,
         y: 20,
-        clipPath: "polygon(50% 0, 50% 0, 50% 100%, 50% 100%)",
       }}
       transition={{ duration: 0.8 }}
       className={classNames("min-h-screenHeightWithoutHeader", className)}
