04278624eca08cb86b330568cebc0ad1807cd444
Jeroen Sak <jeroen.sak@hotmail.com>
2023-01-10 08:23:21 +0100

    remove opacity to prevent 'white flash' on page transition

--- components/page-wrapper/page-wrapper.tsx
+++ components/page-wrapper/page-wrapper.tsx
@@ -11,9 +11,9 @@ export const PageWrapper = ({
   className?: string;
 }) => (
   <motion.div
-    initial={{ opacity: 0, y: 20 }}
+    initial={{ opacity: 1, y: 20 }}
     animate={{ opacity: 1, y: 0 }}
-    exit={{ opacity: 0, y: 20 }}
+    exit={{ opacity: 1, y: 20 }}
     transition={{ duration: 0.3 }}
     className={classNames("min-h-screenHeightWithoutHeader", className)}
   >
